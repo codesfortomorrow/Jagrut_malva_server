9506f2be0c0e099b47fb42a8e00f872190464555
Vikrant Singh <vikrant.mongoosetech@gmail.com>
2023-12-29 17:47:44 +0530

    test: fix open handles

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,7 +1,8 @@
 import Redis from 'ioredis';
-import { Global, Module } from '@nestjs/common';
+import { Queue } from 'bullmq';
+import { Global, Module, OnApplicationShutdown } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { BullModule } from '@nestjs/bullmq';
+import { BullModule, InjectQueue } from '@nestjs/bullmq';
 import * as configs from '@Config';
 import { validateEnvironmentVariables } from './utils';
 import { MailService, StorageService, UtilsService } from './providers';
@@ -20,14 +21,25 @@ const providers = [MailService, StorageService, UtilsService, JwtStrategy];
       load: Object.values(configs),
       validate: validateEnvironmentVariables,
     }),
-    BullModule.registerQueue({
+    BullModule.registerQueueAsync({
       name: MAIL_QUEUE,
-      connection: new Redis(process.env.REDIS_URI as string, {
-        maxRetriesPerRequest: null,
+      useFactory: () => ({
+        connection: new Redis(process.env.REDIS_URI as string, {
+          maxRetriesPerRequest: null,
+        }),
       }),
     }),
   ],
   providers: [...providers, MailProcessor],
   exports: providers,
 })
-export class CommonModule {}
+export class CommonModule implements OnApplicationShutdown {
+  constructor(
+    @InjectQueue(MAIL_QUEUE)
+    private readonly mailQueue: Queue,
+  ) {}
+
+  async onApplicationShutdown() {
+    await this.mailQueue.disconnect();
+  }
+}
